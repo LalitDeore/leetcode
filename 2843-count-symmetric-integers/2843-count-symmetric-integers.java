3efffeba285cd3df2a86ff525cc2df783733ee65
LalitDeore <92305713+LalitDeore@users.noreply.github.com>
2023-10-22 23:25:44 +0530

    Time: 76 ms (11.66%), Space: 43.1 MB (73.74%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public static int digitSum(String digit){
+        int n = Integer.parseInt(digit);
+        int sum = 0;
+        while(n>0){
+           sum = sum + n%10;
+            n = n/10;
+        }
+        return sum;
+    }
+    public int countSymmetricIntegers(int low, int high) {
+       int count = 0;
+        while(low<=high){
+            String value = String.valueOf(low);
+            int len = value.length();
+            if(len%2 == 0){
+                String firstHalf = value.substring(0, len/2);
+                String SecondHalf = value.substring(len/2);
+                if(digitSum(firstHalf)==digitSum(SecondHalf)){
+                    count++;
+                }
+            }
+            low++;
+        }
+        return count;
+    }
+}
